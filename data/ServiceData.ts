eab9aa2fbc2fbfad96407882d688926f457ed2c5
Munna-Khandakar <munnashisad@gmail.com>
2024-11-02 22:41:45 +0600

    service page fixed

---
+++ data/ServiceData.ts
@@ -0,0 +1,46 @@
+import {ServiceType} from "@/types/ServiceType";
+
+export const ServiceData: ServiceType[] = [
+    {
+        id: 1,
+        name: 'Identifying trustworthy',
+        description: 'Identifying trustworthy Japanese manufacturers and suppliers for sourcing quality products, ranging from small components to large machinery',
+        image: '/image/trust.jpg',
+    },
+    {
+        id: 2,
+        name: 'Quality Control and Inspection',
+        description: 'Conducting thorough quality checks on products before shipment to ensure they meet standards.',
+        image: '/image/quality.jpg',
+    },
+    {
+        id: 3,
+        name: 'Logistics and Shipping Coordination',
+        description: 'Managing transportation, customs clearance, and delivery of goods from Japan to Bangladesh.',
+        image: '/image/logistics.jpeg',
+    },
+    {
+        id: 4,
+        name: 'Market Research and Analysis',
+        description: 'Providing insights into market trends, consumer preferences, and competitive analysis in both countries.',
+        image: '/image/market.jpg',
+    },
+    {
+        id: 5,
+        name: 'Technical Support and Consultation',
+        description: 'Offering expertise in specific industries, such as electronics or machinery, to help businesses utilize imported products effectively.',
+        image: '/image/support.jpg',
+    },
+    {
+        id: 6,
+        name: 'After-Sales Service and Support',
+        description: 'Facilitating warranty claims, repairs, and maintenance for imported goods to enhance customer satisfaction',
+        image: '/image/after.jpeg',
+    },
+    {
+        id: 7,
+        name: 'Cultural and Business Etiquette Training',
+        description: 'Assisting Bangladeshi businesses in understanding Japanese business practices and culture for smoother negotiations and partnerships.',
+        image: '/image/training.webp',
+    }
+];
